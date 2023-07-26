b64a2edd5bd1f903530d72ac9e82b9221efc7103
vanh0101 <132863193+vanh0101@users.noreply.github.com>
2023-07-26 10:28:49 +0700

    icloud

---
+++ db.js
@@ -0,0 +1,24 @@
+const { MongoClient } = require('mongodb');
+
+const mongoURI = 'mongodb://127.0.0.1:27017';
+const dbName = 'IT0502';
+
+let db;
+
+async function connectToDatabase() {
+  try {
+    const client = await MongoClient.connect(mongoURI, {
+      useUnifiedTopology: true,
+    });
+    db = client.db(dbName);
+    console.log('Connected to MongoDB successfully!');
+  } catch (err) {
+    console.error('MongoDB connection error:', err);
+  }
+}
+
+function getDatabase() {
+  return db;
+}
+
+module.exports = { connectToDatabase, getDatabase };
